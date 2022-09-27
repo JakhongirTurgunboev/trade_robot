2b90150a33175531123cb61c7a2e838c1eef53b9
Jahongir Turgunboyev <j.turgunboyev@technocorp.uz>
2022-09-27 18:14:05 +0500

    New comnmit

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ request = {
 # send a trading request
 result = mt5.order_send(request)
 # check the execution result
-print("1. order_send(): by {} {} lots at {} with deviation={} points".format(symbol, lot, price, deviation));
+print("1. order_send(): by {} {} lots at {} with deviation={} points".format(symbol, lot, price, deviation))
 if result.retcode != mt5.TRADE_RETCODE_DONE:
     print("2. order_send failed, retcode={}".format(result.retcode))
     # request the result as a dictionary and display it element by element
@@ -153,6 +153,33 @@ else:
             traderequest_dict = result_dict[field]._asdict()
             for tradereq_filed in traderequest_dict:
                 print("       traderequest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
+
+pd.set_option('display.max_columns', 500)  # number of columns to be displayed
+pd.set_option('display.width', 1500)  # max table width to display
+
+# establish connection to MetaTrader 5 terminal
+if not mt5.initialize():
+    print("initialize() failed, error code =", mt5.last_error())
+    quit()
+
+# get 10 GBPUSD D1 bars from the current day
+rates = mt5.copy_rates_from_pos("GBPUSD", mt5.TIMEFRAME_M30, 0, 10)
+
+# shut down connection to the MetaTrader 5 terminal
+mt5.shutdown()
+# display each element of obtained data in a new line
+print("Display obtained data 'as is'")
+for rate in rates:
+    print(rate)
+
+# create DataFrame out of the obtained data
+rates_frame = pd.DataFrame(rates)
+# convert time in seconds into the datetime format
+rates_frame['time'] = pd.to_datetime(rates_frame['time'], unit='s')
+
+# display data
+print("\nDisplay dataframe with data")
+print(rates_frame)
 
 # shut down connection to the MetaTrader 5 terminal
 mt5.shutdown()
